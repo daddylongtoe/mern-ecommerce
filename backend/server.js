789be3de78b473362376fee5995c1c5ac26065ca
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2021-04-15 16:29:05 +0100

    Create custom error handler and related middleware

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ import 'dotenv/config.js';
 import express from 'express';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 connectDB();
 
 const app = express();
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
